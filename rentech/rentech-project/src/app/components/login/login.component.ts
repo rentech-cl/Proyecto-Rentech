b26bccd154e2527b1814a895adcaae84989e8c80
DAW2 <ggomezayuso@gmail.com>
2021-04-21 19:53:52 +0200

    login front

--- rentech/rentech-project/src/app/components/login/login.component.ts
+++ rentech/rentech-project/src/app/components/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css','../home/home.component.css']
 })
 export class LoginComponent implements OnInit {
 

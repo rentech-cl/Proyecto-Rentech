8e2c3459fde2234e6fd12c39f8b0114a2da12bc1
Rahul-steez <rmanwanim@gmail.com>
2021-04-21 20:04:04 +0200

    Creacion Registro

--- rentech/rentech-project/src/app/app-routing.module.ts
+++ rentech/rentech-project/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component:LoginComponent},
+  { path: 'registro', component:RegisterComponent},
   { path: 'home', component: HomeComponent} , //HOME es un componente que requiere ser identificado, debido a esto, le introducimos un "guardian" para controlar su acceso
   { path: '**', redirectTo: 'home' },
 

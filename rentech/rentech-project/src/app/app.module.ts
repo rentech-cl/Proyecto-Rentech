a9a85b53a9f38e2fb6ce2b8a30719b70e1d1dd9d
DAW2 <ggomezayuso@gmail.com>
2021-04-22 15:39:09 +0200

    login front hecho

--- rentech/rentech-project/src/app/app.module.ts
+++ rentech/rentech-project/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

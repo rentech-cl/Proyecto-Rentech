a2170da8e8bfdc46efb2f9366262aec09ac052d9
DAW2 <ggomezayuso@gmail.com>
2021-04-27 20:01:15 +0200

    login y register funcional

--- rentech/new_rentech/src/app/pages/index/index.component.ts
+++ rentech/new_rentech/src/app/pages/index/index.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Cliente } from 'src/app/models/cliente';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfirmedValidator } from 'src/app/confirmed.validator';
+import { ClienteService } from '../../services/cliente.service';
 
 
 @Component({
@@ -19,13 +21,15 @@ export class IndexComponent implements OnInit {
   myForm: FormGroup;
   usuario = new Cliente;
   registerForm: FormGroup;
+  submitted = false;
 
   currentSection = 'home';
 
   constructor(
     private modalService: NgbModal,
     public formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private ClienteService: ClienteService
     ) {
 
     }
@@ -36,6 +40,20 @@ export class IndexComponent implements OnInit {
       contrasena: ['', [Validators.minLength(2), Validators.maxLength(15), Validators.required]],
     }
     );
+    this.registerForm = this.formBuilder.group({
+      nombre: ['', [Validators.minLength(2), Validators.maxLength(15), Validators.required]],
+      apellido: ['', [Validators.minLength(2), Validators.maxLength(15), Validators.required]],
+      correo: ['', [Validators.email, Validators.required]],
+      telefono: ['', [ Validators.required, Validators.pattern("[0-9 ]{9}")]],
+      iban: ['', [Validators.minLength(24), Validators.maxLength(24), Validators.required]],
+      dni: ['', [Validators.required, Validators.pattern("^[0-9]{8}[A-Za-z]$")]],
+      cp: ['', [Validators.required, Validators.pattern("[0-9 ]{5}")]],
+      direccio: ['', [Validators.minLength(2), Validators.maxLength(60), Validators.required]],
+      contrasena: ['', [Validators.minLength(2), Validators.maxLength(15), Validators.required]],
+      confirm_password: [null, Validators.required],
+    }, {
+      validator: ConfirmedValidator('contrasena', 'confirm_password')
+    });
   }
 
   /**
@@ -85,5 +103,16 @@ export class IndexComponent implements OnInit {
    */
   registerModal(registercontent) {
     this.modalService.open(registercontent, { centered: true });
+  }
+
+  register() {
+    this.submitted = true;
+    console.log(this.registerForm.value);
+    if (this.registerForm.invalid) {
+      return;
+    }else{
+      console.log("Has sido registrado!!");
+      this.ClienteService.register(this.registerForm.value);
+    }
   }
 }

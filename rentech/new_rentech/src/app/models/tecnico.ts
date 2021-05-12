23de614dc6bbc227252c36fa62dc5f4749f2fa53
Lomishi <jordicolellferrer89@gmail.com>
2021-05-12 17:43:55 +0200

    tecnico 1.1
    tecnico

---
+++ rentech/new_rentech/src/app/models/tecnico.ts
@@ -0,0 +1,11 @@
+export class Tecnico {
+  dni?: string;
+  nombre?: string;
+  apellido?: string;
+  correo?: string;
+  telefono?: string;
+  direccio?: string;
+  salario?: string;
+  iban?: string;
+  contrasena?: string;
+}

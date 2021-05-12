d45bb610989d84b3759dc1257a5133706d564c9e
JorgeIs-01 <infantessorolla@gmail.com>
2021-05-12 17:42:50 +0200

    creacion php y coreccion consulta

---
+++ rentech/new_rentech/src/app/php/asignaraveria.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'database.php';
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+$texto = file_get_contents("php://input");
+$jsonasignar = json_decode($texto);
+
+if(!$jsonaveria){
+  exit("No hay datos");
+}
+
+else{
+  //si no coinciden campos vitales para que se pueda controlar correctamente u cliente haremos el insert a la base de datos
+
+  $sentencia ="INSERT INTO `averias`(`idEmpleado`)
+  VALUES (                                      '$jsonaveria->idempleado'
+                                                )";
+  if ($res = mysqli_query($con,$sentencia)) {
+
+    echo('{ "result": "OK" }');
+
+  } else{
+      echo('{ "result": "ERROR", "message": "Algo salio mal"  }');
+    }
+}
+
+?>
